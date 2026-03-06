7c13facf70478243c122c27b3d3b1841b4425cf9
coder-utshab <ghoshutshob470@gmail.com>
2026-03-07 00:43:41 +0530

    up

--- explore-react-core-concepts/src/App.jsx
+++ explore-react-core-concepts/src/App.jsx
@@ -3,92 +3,148 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './Todo'
+import Actor from './Actor'
+import Singer from './singer'
 
 function App() {
- 
+
+  // actors data
+  const actors = [
+    'sajib',
+    'avijeet roy',
+    'shuvo',
+    'dipto'
+  ];
+
+  // singers data
+  const singers = [
+    {id: 1, name: 'Dr. Mahfuzur Rahman', age: 60 },
+    {id:2, name: 'Eva Rahaman', age: 38 },
+    { id:3, name: 'Shuvro Dev', age: 60 },
+    {id:4, name: 'Pritom Vai', age: 26},
+  ];
+
   return (
     <>
-     <h3>Vite + React</h3>
+      <h2>Vite + React</h2>
+
+
+{/* singer.jsx--------------------------------------- */}
+      {
+  singers.map(singer =>
+    <Singer key={singer.id} singer={singer}></Singer>
+  )
+}
+
+ 
+
+
+
+      {/* actors.jsx----------------------------- */}
+     <Actor name={"Utshab Kumar"}></Actor>
+      {
+        actors.map(actor =>
+          <Actor key={actor} name={actor}></Actor>
+        )
+      }
+
+
 
      {/* atar output ar jonno amra todo.js ar moddo pabo */}
 
-     {/* <Todo
-      task="Learn Rreact" 
-      isDone={true}></Todo>
-      <Todo
-      task="Explore Core Concepts"
-       isDone={false}></Todo>
-      <Todo
-       task="Try JSX" 
-       isDone={true}></Todo> */}
+<Todo 
+  task="Learn React" 
+  isDone={true}>
+</Todo>
+
+<Todo 
+  task="Explore Core Concepts" 
+  isDone={false}>
+</Todo>
+
+<Todo 
+  task="Try JSX" 
+  isDone={true}>
+</Todo>
 
 
 
-{/* // agula display ta dakta gala neshar function gula folow korbo */}
 
-     {/* <Device name="Laptop" price="5500"></Device>
-     <Device name="Mobaile" price="2000"></Device>
-     <Device name="Watch" price="1500"></Device>
-     <Person></Person>
-     <Student grade="7" score="99"></Student>
-     <Student grade={12} score="85"></Student>
-     <Student></Student>
-     <Devoloper></Devoloper> */}
+{/* agula display ta dakta gala neshar function gula follow korbo */}
+
+<Device name="Laptop" price="5500"></Device>
+<Device name="Mobile" price="2000"></Device>
+<Device name="Watch" price="1500"></Device>
+
+<Person></Person>
+
+<Student grade="7" score="99"></Student>
+<Student grade={12} score="85"></Student>
+<Student></Student>
+
+<Devoloper></Devoloper>
+
     </>
   )
 }
 
+
+
+//---------------------------------------------------------
+
 function Device(props){
-  // properti ka short kora props bola hocca
   console.log(props)
-  return <h2>This Device:{props.name} price: {props.price}</h2>
+  return <h2>This Device: {props.name} price: {props.price}</h2>
 }
 
 
-const {grade, score} = {grade: '7', score: '99'};
+// Student Component
 
 function Student({grade=1, score=0}){
   console.log(grade, score)
+
   return ( 
-   <div className='student'>
-    <h3>This is a student</h3>
-    <p>Name: {grade}</p>
-    <p>score: {score}</p>
-    <p>age:</p>
-  </div>
+    <div className='student'>
+      <h3>This is a student</h3>
+      <p>Grade: {grade}</p>
+      <p>Score: {score}</p>
+      <p>Age:</p>
+    </div>
   )
 }
 
+
+
+// Person Component
 
 function Person(){
   const age = 25;
   const money = 20;
   const person = {name: 'sakib', age: 12}
-  return <h3>I am a {person.name} with age: {age + money}</h3>
+
+  return <h3>I am {person.name} with age: {age + money}</h3>
 }
 
-// function Student(){
-//   return ( 
-//    <div className='student'>
-//     <h3>This is a student</h3>
-//     <p>Name:</p>
-//     <p>age:</p>
-//   </div>
-//   )
-// }
+
+
+// Developer Component
 
 function Devoloper(){
+
   const devoloperstyle = {
     margin:'20px',
     padding:'20px',
     border:'2px solid purple',
-    borderRadious: '20px'
+    borderRadius:'20px'
   }
+
   return (
     <div style={devoloperstyle}>
-      <h5>dev devo</h5>
+      <h5>Dev Devo</h5>
       <p>Coding</p>
     </div>
   )
 }
+
+
 export default App
---
+++ explore-react-core-concepts/src/singer.jsx
@@ -0,0 +1,9 @@
+export default function Singer({singer}){
+  console.log(singer);
+  return (
+    <div>
+        <h3>Singer:</h3>
+        <p>age:</p>
+    </div>
+  )
+}
